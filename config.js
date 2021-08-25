381f72b1c1f92ee8f43269baa7304485c7b4f988
Nikhil Menon <74824711+nikhil9302@users.noreply.github.com>
2021-08-25 20:18:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 module.exports = {
-    consumer_key: process.env.CONSUMER_KEY,
+   	consumer_key: process.env.CONSUMER_KEY,
 	consumer_secret: process.env.CONSUMER_SECRET,
 	access_token_key: process.env.ACCESS_TOKEN_KEY,
 	access_token_secret: process.env.ACCESS_TOKEN_SECRET
